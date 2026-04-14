768fc371237f3604d6ad7ecbe4663867ca2c9905
thanatos123990 <juanpablourrego123@gmail.com>
2026-04-14 09:08:13 -0500

    Little_Fix

--- Docker-Microservices/services/auth_service/app/config.py
+++ Docker-Microservices/services/auth_service/app/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-PROJECT_ROOT = Path(__file__).resolve().parents[3]
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DEFAULT_USERS_CSV = PROJECT_ROOT / "data" / "users.csv"
 
 class Config:
